f597b14f3f956c1000f3673bd574cd58e2649995
Saurabh kumar <learner.saurabh.sks@gmail.com>
2024-09-04 17:28:57 +0530

    printing patterns

---
+++ patterns.js
@@ -0,0 +1,103 @@
+/*
+
+*****
+ *****
+ *****
+ *****
+ *****
+
+ */
+
+// for (let i = 0; i < 5; i++) {
+//   console.log("*****");
+// }
+
+/*
+
+ *
+ **
+ ***
+ ****
+ *****
+
+ */
+
+// for (let i = 1; i <= 5; i++) {
+//   let str = "";
+//   for (let j = 1; j <= i; j++) {
+//     str += "*";
+//   }
+//   console.log(str);
+// }
+
+/*
+
+ 1
+ 12
+ 123
+ 1234
+ 12345
+
+ */
+
+// for (let i = 1; i <= 5; i++) {
+//   let str = "";
+//   for (let j = 1; j <= i; j++) {
+//     str += j;
+//   }
+//   console.log(str);
+// }
+
+/*
+
+ 1
+ 22
+ 333
+ 4444
+ 55555
+
+ */
+
+// for (let i = 1; i <= 5; i++) {
+//   let str = "";
+//   for (let j = 1; j <= i; j++) {
+//     str += i;
+//   }
+//   console.log(str);
+// }
+
+/*
+
+ *****
+ ****
+ ***
+ **
+ *
+
+ */
+
+// for (let i = 5; i >= 1; i--) {
+//   let str = "";
+//   for (let j = 1; j <= i; j++) {
+//     str += "*";
+//   }
+//   console.log(str);
+// }
+
+/*
+
+12345
+1234
+123
+12
+1
+
+ */
+
+// for (let i = 5; i >= 1; i--) {
+//   let str = "";
+//   for (let j = 1; j <= i; j++) {
+//     str += j;
+//   }
+//   console.log(str);
+// }
